a58bbecf9274a43ac115056af28de0a3a36b1036
mgonzalezp <mgonzalezp@fi.uba.ar>
2025-08-30 13:14:07 -0300

    readaptación para sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-CONFIG_FILE="./server/config.ini"
 NETWORK_NAME="tp0_testing_net"
 MESSAGE="Hola Mundo"
 
-# Leer SERVICE_NAME y PORT desde el archivo INI usando Python
-read SERVICE_NAME PORT < <(
-python3 - <<EOF
-import configparser
-config = configparser.ConfigParser()
-config.read('$CONFIG_FILE')
-print(config['DEFAULT']['SERVER_IP'], config['DEFAULT']['SERVER_PORT'])
-EOF
-)
+# Leer SERVICE_NAME y PORT desde el archivo INI usando el script de Python
+output=$(python3 read_srv_config.py)
+
+# Asignar los valores de la salida a las variables SERVICE_NAME y PORT
+SERVICE_NAME=$(echo $output | cut -d' ' -f1)
+PORT=$(echo $output | cut -d' ' -f2)
 
 # Contenedor temporal para enviar mensaje usando busybox
 RESPONSE=$(docker run --rm --network $NETWORK_NAME busybox sh -c \
     "echo '$MESSAGE' | nc $SERVICE_NAME $PORT")
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
     exit 0
 else
