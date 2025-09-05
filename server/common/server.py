de48818b4d3a7a08ce8124b0bf03166f5e8f4da9
mgonzalezp <mgonzalezp@fi.uba.ar>
2025-09-04 21:37:42 -0300

    refactor: replace struct unpacking with from_bytes

--- server/common/server.py
+++ server/common/server.py
@@ -15,7 +15,7 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
-        # Initialize server struct
+        # Initialize server state
         self.client_amount = int(client_amount)
         
         file_lock = threading.Lock()
